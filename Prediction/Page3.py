e5be0d3d488a276beae92003f6a7d53a472ce1d6
taoting <335354289@qq.com>
2020-05-23 10:02:35 +0800

    delete all

--- Prediction/Page3.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-from ui.page3 import Ui_Form
-
-
-class page3_window(QtWidgets.QWidget):
-    def __init__(self):
-        QtWidgets.QWidget.__init__(self)
-        self.ui = Ui_Form()
-        self.ui.setupUi(self)
