3dc001ebffa2127d1d8fbc2b5824cee4b35ed105
Jiubei0408 <757795210@qq.com>
2020-05-26 23:47:39 +0800

    add linkManager

--- ui/widgets/MainPage/__init__.py
+++ ui/widgets/MainPage/__init__.py
@@ -10,15 +10,17 @@ class MainPage(QWidget):
         self.ui.setupUi(self)
         self.init_navigate_menu(self.ui.navigation_widget)
 
-    @staticmethod
-    def init_navigate_menu(widget) -> NavigateMenu:
+    def linkManager(self, s: str):
+        print(s)
+
+    def init_navigate_menu(self, widget) -> NavigateMenu:
         menu = [
             ("信息登记", ["来绍交流", "台商台干", "重要人士", "陆配", "陆生", "台属"]),
             ("地区统计", ["测试文本1", "测试文本2"])
         ]
         for i in menu:
             j, k = i
-            widget.addField(j, [(kk, None) for kk in k])
+            widget.addField(j, [(kk, self.linkManager) for kk in k])
         widget.fields[0].switch()
         widget.fields[0].menu_labels[0].linkActivated.emit("#")
         return widget
