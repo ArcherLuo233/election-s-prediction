512015b729a8b6a528d778bbbf2db9b8e5a064d8
Jiubei0408 <757795210@qq.com>
2020-05-27 12:43:20 +0800

    添加窗口管理器

--- qt_app.py
+++ qt_app.py
@@ -2,3 +2,18 @@ from PyQt5 import QtWidgets
 import sys
 
 app = QtWidgets.QApplication(sys.argv)
+
+
+class PageManager:
+    pages = dict()
+
+    @classmethod
+    def getPage(cls, page_name):
+        print(page_name)
+        if page_name in cls.pages.keys():
+            return cls.pages[page_name]
+        if 'Page' + page_name in globals().keys():
+            cls.pages[page_name] = globals()['Page' + page_name]
+            return cls.pages[page_name]
+        print("Can't find page: %s" % page_name)
+        return None
