f6e8a20ee932381e807f5d15ab8b184b7e4e30cf
Jiubei0408 <757795210@qq.com>
2020-05-30 19:32:31 +0800

    应用详细信息对话框

--- ui/widgets/subpages/Page1_1/__init__.py
+++ ui/widgets/subpages/Page1_1/__init__.py
@@ -1,18 +1,68 @@
-from PyQt5.QtWidgets import QWidget, QHeaderView
+from PyQt5.QtWidgets import QWidget, QHeaderView, QLabel
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QEventLoop, QRect
 
 from .pageUI import Ui_Form
+from .detailPage import DetailPage
+from ui.page_elements.WindowMask import WindowMask
 
 
 class Page1_1(QWidget):
     def __init__(self):
         QWidget.__init__(self)
+        self.dialog = None
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        hor_header:QHeaderView = self.ui.tableWidget.horizontalHeader()
+        hor_header: QHeaderView = self.ui.tableWidget.horizontalHeader()
         hor_header.setFixedHeight(30)
         hor_header.setSectionResizeMode(QHeaderView.Stretch)
         self.ui.tableWidget.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
         icon = QIcon("./static/svg/search.svg")
         self.ui.button_search.setIcon(icon)
         self.ui.page_controller.setMaxPage(10)
+
+        self.ui.button_add.clicked.connect(self.action_add)
+
+        # test label
+        detail_label = QLabel(self)
+        detail_label.setText('<a href="#detail:%d">详细信息</a>' % (0))
+        detail_label.linkActivated.connect(self.detail)
+        detail_label.show()
+
+        self.ui.tableWidget.setCellWidget(0, 5, detail_label)
+
+    def detail(self, link):
+        print(link)
+        self.openDialog(False, data={'id': link[len("#detail:"):]})
+
+    def action_add(self):
+        self.openDialog(True)
+
+    def openDialog(self, enable: bool, data=None):
+        mask = WindowMask(self)
+        self.dialog = DetailPage()
+        self.dialog.setParent(self)
+        self.locationDialog()
+        self.dialog.ui.tableWidget.setEnabled(enable)
+        loop = QEventLoop()
+        mask.clicked.connect(loop.quit)
+        mask.show()
+        self.dialog.show()
+        loop.exec()
+        self.dialog.close()
+        mask.close()
+        self.dialog.deleteLater()
+        self.dialog = None
+        mask.deleteLater()
+
+    def resizeEvent(self, QResizeEvent):
+        self.locationDialog()
+
+    def locationDialog(self):
+        if self.dialog:
+            geo = self.geometry()
+            geo.setLeft(geo.left() + 50)
+            geo.setRight(geo.right() - 50)
+            geo.setTop(geo.top() + 30)
+            geo.setBottom(geo.bottom() - 50)
+            self.dialog.setGeometry(geo)
