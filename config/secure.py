a556dbda521aca04561f284d9bff73e756e5abf5
taoting <335354289@qq.com>
2020-06-03 22:04:21 +0800

    修改开发数据库为mysql

--- config/secure.py
+++ config/secure.py
@@ -1,5 +1,6 @@
 # 配置数据库信息
-SQLALCHEMY_URL = 'sqlite:///database.db'
+# SQLALCHEMY_URL = 'sqlite:///database.db'
+SQLALCHEMY_URL = 'mysql+pymysql://election:ele123ction@newitd-w.mysql.rds.aliyuncs.com/election'
 
 # 配置密码加密的盐
 PASSWORD_SALT = '123123'
