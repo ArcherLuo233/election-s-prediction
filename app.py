f299e3def890425589e30792b0716e389082984c
Jiubei0408 <757795210@qq.com>
2020-06-03 23:15:26 +0800

    添加debug模式

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from model.base import init_database
 from ui.widgets.LoginPage import LoginPage
 from ui.widgets.MainPage import MainPage
 from libs.PageManager import PageManager
+from libs.g import g
 
 app = QtWidgets.QApplication(sys.argv)
 
 
 def main():
+    g.debug = True
     login_widget = LoginPage()
     PageManager.pages['Login'] = login_widget
     main_widget = MainPage()
