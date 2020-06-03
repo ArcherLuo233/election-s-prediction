5db0f24472acf7502954885445f7135cee59a180
Jiubei0408 <757795210@qq.com>
2020-06-04 00:10:39 +0800

    添加字体库以及加载

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ from model.base import init_database
 from ui.widgets.LoginPage import LoginPage
 from ui.widgets.MainPage import MainPage
 from libs.PageManager import PageManager
+from libs.fonts import loadFonts
 from libs.g import g
 
 app = QtWidgets.QApplication(sys.argv)
+g.app = app
 
 
 def main():
     g.debug = True
+    loadFonts()
     login_widget = LoginPage()
     PageManager.pages['Login'] = login_widget
     main_widget = MainPage()
