e2cca7e920caad95c56c33e072df1f3e51fd42c9
Jiubei0408 <757795210@qq.com>
2020-06-04 12:23:33 +0800

    添加字体库调试信息

--- libs/fonts.py
+++ libs/fonts.py
@@ -5,6 +5,8 @@ from PyQt5.QtCore import QDir
 def loadFonts():
     fonturl = QDir.currentPath() + "/static/fonts/"
     files = QDir(fonturl).entryList(("*.ttf", "*.ttc"))
+    print(files)
     for file in files:
         filename = fonturl + file
-        QFontDatabase.addApplicationFont(filename)
+        id = QFontDatabase.addApplicationFont(filename)
+        print(file, id)
