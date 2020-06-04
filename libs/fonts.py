4ae15f2784a70adc1ac2f3010115690006385d48
Jiubei0408 <757795210@qq.com>
2020-06-04 12:36:20 +0800

    删除调试信息

--- libs/fonts.py
+++ libs/fonts.py
@@ -5,9 +5,6 @@ from PyQt5.QtCore import QDir
 def loadFonts():
     fonturl = QDir.currentPath() + "/static/fonts/"
     files = QDir(fonturl).entryList(("*.ttf", "*.ttc"))
-    print(files)
     for file in files:
         filename = fonturl + file
-        id = QFontDatabase.addApplicationFont(filename)
-        print(file, id)
-    print(QFontDatabase().families(QFontDatabase.SimplifiedChinese))
+        QFontDatabase.addApplicationFont(filename)
