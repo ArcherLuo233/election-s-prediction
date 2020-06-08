1dcc82ef7ceaed0e1cf57c74560be83d39c80ac9
Jiubei0408 <757795210@qq.com>
2020-06-08 19:51:19 +0800

    jzz改为不需要照片

--- ui/widgets/subpages/Page1_10/__init__.py
+++ ui/widgets/subpages/Page1_10/__init__.py
@@ -5,6 +5,7 @@ from ui.widgets.subpages.Page1_x import Page1_x
 class Page1_10(Page1_x):
     model = JZZ
     summary = {'姓名': 'nickname', '性别': 'sex', '身份证号码': 'id_card'}
+    need_pic = False
 
     def __init__(self):
         Page1_x.__init__(self, "居住证人员", "jzz")
