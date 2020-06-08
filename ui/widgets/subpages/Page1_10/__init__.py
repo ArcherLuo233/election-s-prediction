02fb37b5cb9333ccf7b0dcb3ee25e108b897f1b7
Jiubei0408 <757795210@qq.com>
2020-06-08 19:05:26 +0800

    增加显示所有记录 增加summary功能，在初始页面显示记录的简明信息

--- ui/widgets/subpages/Page1_10/__init__.py
+++ ui/widgets/subpages/Page1_10/__init__.py
@@ -4,6 +4,7 @@ from ui.widgets.subpages.Page1_x import Page1_x
 
 class Page1_10(Page1_x):
     model = JZZ
+    summary = {'姓名': 'nickname', '性别': 'sex', '身份证号码': 'id_card'}
 
     def __init__(self):
         Page1_x.__init__(self, "居住证人员", "jzz")
