6dc31ff5c878a02ea226cb075df8538799bbd5e6
Jiubei0408 <757795210@qq.com>
2020-06-08 19:51:48 +0800

    增加tstg

--- ui/widgets/subpages/Page1_2/__init__.py
+++ ui/widgets/subpages/Page1_2/__init__.py
@@ -1,6 +1,13 @@
 from ui.widgets.subpages.Page1_x import Page1_x
+from model.tstg import TSTG
 
 
 class Page1_2(Page1_x):
+    model = TSTG
+    summary = {
+        '姓名': 'nickname',
+        '性别': 'sex'
+    }
+
     def __init__(self):
         Page1_x.__init__(self, "台商台干", "")
