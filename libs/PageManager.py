66abbee1f5d30dc432344a33e5a7204a13d53beb
ArcherLuo233 <1076635323@qq.com>
2020-06-19 16:01:39 +0800

    地区统计页面导入

--- libs/PageManager.py
+++ libs/PageManager.py
@@ -12,6 +12,11 @@ from ui.widgets.subpages.Page1_10 import Page1_10
 from ui.widgets.subpages.Page3 import Page3
 from ui.widgets.subpages.Page3_1 import Page3_1
 from ui.widgets.subpages.Page4 import Page4
+from ui.widgets.subpages.Page2_1 import Page2_1
+from ui.widgets.subpages.Page2_2 import Page2_2
+
+
+
 
 
 class PageManager:
