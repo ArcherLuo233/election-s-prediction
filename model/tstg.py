e5880f3840caf20ee8c9b4aa4b724591301e727f
taoting <335354289@qq.com>
2020-06-22 15:50:56 +0800

    修改字段类型

--- model/tstg.py
+++ model/tstg.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, LargeBinary
 
 from model.base import Base
 
@@ -19,7 +19,7 @@ class TSTG(Base):
     template_start_row = 4
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    photo = Column(String(100), comment='照片')
+    photo = Column(LargeBinary, comment='照片')
     nickname = Column(String(100), nullable=False, comment='姓名')
     sex = Column(String(100), comment='性别')
     birth = Column(String(100), comment='出生日期')
