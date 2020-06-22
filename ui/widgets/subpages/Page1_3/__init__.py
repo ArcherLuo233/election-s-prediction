b2069a40356588dbb215d279bedea91417c7084d
Jiubei0408 <757795210@qq.com>
2020-06-22 13:49:44 +0800

    合并page1的子页面，清理一堆文件

--- ui/widgets/subpages/Page1_3/__init__.py
+++
@@ -1,6 +0,0 @@
-from ui.widgets.subpages.Page1_x import Page1_x
-
-
-class Page1_3(Page1_x):
-    def __init__(self):
-        Page1_x.__init__(self, "重要人士", "")
--- ui/widgets/subpages/Page1_9/__init__.py
+++
@@ -1,6 +0,0 @@
-from ui.widgets.subpages.Page1_x import Page1_x
-
-
-class Page1_9(Page1_x):
-    def __init__(self):
-        Page1_x.__init__(self, "来访团组", "")
