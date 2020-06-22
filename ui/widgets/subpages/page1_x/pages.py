159be343e9b264d97ce2e47003c95b9f2d1f0c71
Jiubei0408 <757795210@qq.com>
2020-06-23 01:03:01 +0800

    修改部分page名字

--- ui/widgets/subpages/page1_x/pages.py
+++ ui/widgets/subpages/page1_x/pages.py
@@ -9,7 +9,7 @@ class Page1_1(Page1_x):
     title = "来绍交流"
 
 
-class Page1_2(Page1_x):
+class Pagetstg(Page1_x):
     title = "台商台干"
     model = TSTG
     summary = {
@@ -23,7 +23,7 @@ class Page1_3(Page1_x):
     title = "重要人士"
 
 
-class Page1_4(Page1_x):
+class Pagelp(Page1_x):
     title = "陆配"
     model = LP
     summary = {
@@ -37,7 +37,7 @@ class Page1_5(Page1_x):
     title = "陆生"
 
 
-class Page1_6(Page1_x):
+class Pagets(Page1_x):
     title = "台属"
     model = TS
     summary = {
@@ -59,7 +59,7 @@ class Page1_9(Page1_x):
     title = "来访团组"
 
 
-class Page1_10(Page1_x):
+class Pagejzz(Page1_x):
     title = "居住证人员"
     model = JZZ
     summary = {'姓名': 'nickname', '性别': 'sex', '身份证号码': 'id_card'}
