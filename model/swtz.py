54bb2ef834caba37a5bd4d5d0287a6e60695b253
taoting <335354289@qq.com>
2020-06-22 12:17:34 +0800

    新增swtz

---
+++ model/swtz.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from model.base import Base
+
+from model.swtz_ty import SWTZ_TY
+
+
+# 商务团组
+class SWTZ(Base):
+    __tablename__ = 'swtz'
+
+    field = [
+        'id', 'company_name', 'main_business', 'datetime', 'reason', 'taiwan_company_name', 'taiwan_main_business',
+        'taiwan_company_address', 'taiwan_company_legal_people', 'members'
+    ]
+
+    template_filename = ''
+    template_start_row = 0
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    company_name = Column(String(100), comment='企业名称')
+    main_business = Column(String(100), comment='主营业务')
+    datetime = Column(String(100), comment='时间')
+    reason = Column(String(100), comment='赴台事由及行程')
+    taiwan_company_name = Column(String(100), comment='台方企业名称')
+    taiwan_main_business = Column(String(100), comment='台方主营业务')
+    taiwan_company_address = Column(String(100), comment='台方企业地址')
+    taiwan_company_legal_people = Column(String(100), comment='台方企业法人代表')
+    members = relationship(SWTZ_TY)
---
+++ model/swtz_ty.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from model.base import Base
+
+
+# 商务团组_团员
+class SWTZ_TY(Base):
+    __tablename__ = 'swtz_ty'
+
+    field = [
+        'id', 'nickname', 'job', 'id_card', 'phone'
+    ]
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    swtz_id = Column(Integer, ForeignKey('swtz.id'))
+    nickname = Column(String(100), comment='姓名')
+    job = Column(String(100), comment='单位职务')
+    id_card = Column(String(100), comment='身份证号')
+    phone = Column(String(100), comment='联系电话')
