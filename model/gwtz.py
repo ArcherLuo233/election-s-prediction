0e5176763c60d3f2b9d4a3db872f143b1ca1dfc4
taoting <335354289@qq.com>
2020-06-22 16:48:06 +0800

    新增公务团组

---
+++ model/gwtz.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, LargeBinary, String
+from sqlalchemy.orm import relationship
+
+from model.base import Base
+from model.gwtz_ty import GWTZ_TY
+
+
+# 公务团组
+class GWTZ(Base):
+    __tablename__ = 'gwtz'
+
+    field = [
+        'id', 'year', 'name', 'company', 'taiwan_invite_company', 'number_of_people', 'number_of_day', 'approval_time',
+        'actual_time', 'stroke', 'necessity_statement', 'summary', 'contact_person', 'contact_phone',
+    ]
+
+    template_filename = ''
+    template_start_row = 0
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    year = Column(String(100), comment='年度')
+    name = Column(String(100), comment='团组名称')
+    company = Column(String(100), comment='组团单位')
+    taiwan_invite_company = Column(String(100), comment='台湾邀请单位')
+    number_of_people = Column(String(100), comment='人数')
+    number_of_day = Column(String(100), comment='天数')
+    approval_time = Column(String(100), comment='报批赴台时间')
+    actual_time = Column(String(100), comment='实际赴台时间')
+    stroke = Column(LargeBinary, comment='行程')
+    necessity_statement = Column(LargeBinary, comment='必要性说明')
+    summary = Column(LargeBinary, comment='总结')
+    contact_person = Column(String(100), comment='联系人')
+    contact_phone = Column(String(100), comment='联系电话')
+    members = relationship(GWTZ_TY)
---
+++ model/gwtz_ty.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, ForeignKey, Integer, LargeBinary, String
+
+from model.base import Base
+
+
+# 公务团组_团员
+class GWTZ_TY(Base):
+    __tablename__ = 'gwtz_ty'
+
+    field = [
+        'id', 'nickname', 'sex', 'job', 'type', 'nature', 'taiwan_position', 'entry_number', 'pass_number',
+        'filing_form'
+    ]
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    gwtz_id = Column(Integer, ForeignKey('gwtz.id'))
+    nickname = Column(String(100), comment='姓名')
+    sex = Column(String(100), comment='性别')
+    job = Column(String(100), comment='单位职务')
+    type = Column(String(100), comment='人物类型')
+    nature = Column(String(100), comment='人员性质')
+    taiwan_position = Column(String(100), comment='赴台身份及职务')
+    entry_number = Column(String(100), comment='入台证号')
+    pass_number = Column(String(100), comment='通行证号')
+    filing_form = Column(LargeBinary, comment='备案表')
