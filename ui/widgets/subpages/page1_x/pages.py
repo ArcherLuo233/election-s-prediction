038957d2908fed2946dc431b4497290ba7d3eed0
Jiubei0408 <757795210@qq.com>
2020-06-23 20:08:46 +0800

    添加团组前端（部分完成）

--- ui/widgets/subpages/page1_x/pages.py
+++ ui/widgets/subpages/page1_x/pages.py
@@ -1,7 +1,10 @@
+from model.gwtz import GWTZ
 from model.jzz import JZZ
+from model.lftz import LFTZ
 from model.lp import LP
 from model.ls import LS
 from model.lsjl import LSJL
+from model.swtz import SWTZ
 from model.ts import TS
 from model.tstg import TSTG
 from model.zyrs import ZYRS
@@ -55,14 +58,17 @@ class Pagets(Page1_x):
 
 class Pagegwtz(Page1_x):
     title = "公务团组"
+    model = GWTZ
 
 
 class Pageswtz(Page1_x):
     title = "商务团组"
+    model = SWTZ
 
 
 class Pagelftz(Page1_x):
     title = "来访团组"
+    model = LFTZ
 
 
 class Pagejzz(Page1_x):
