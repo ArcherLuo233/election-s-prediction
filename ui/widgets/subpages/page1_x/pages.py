7b5bc11e59445b80c64c4d54a40503d580899259
Jiubei0408 <757795210@qq.com>
2020-06-23 13:27:26 +0800

    非团前端

--- ui/widgets/subpages/page1_x/pages.py
+++ ui/widgets/subpages/page1_x/pages.py
@@ -2,11 +2,15 @@ from model.jzz import JZZ
 from model.lp import LP
 from model.ts import TS
 from model.tstg import TSTG
+from model.lsjl import LSJL
+from model.zyrs import ZYRS
+from model.ls import LS
 from ui.widgets.subpages.page1_x import Page1_x
 
 
-class Page1_1(Page1_x):
+class Pagelsjl(Page1_x):
     title = "来绍交流"
+    model = LSJL
 
 
 class Pagetstg(Page1_x):
@@ -19,8 +23,9 @@ class Pagetstg(Page1_x):
     need_pic = True
 
 
-class Page1_3(Page1_x):
+class Pagezyrs(Page1_x):
     title = "重要人士"
+    model = ZYRS
 
 
 class Pagelp(Page1_x):
@@ -33,8 +38,9 @@ class Pagelp(Page1_x):
     }
 
 
-class Page1_5(Page1_x):
+class Pagels(Page1_x):
     title = "陆生"
+    model = LS
 
 
 class Pagets(Page1_x):
@@ -47,19 +53,23 @@ class Pagets(Page1_x):
     }
 
 
-class Page1_7(Page1_x):
+class Pagegwtz(Page1_x):
     title = "公务团组"
 
 
-class Page1_8(Page1_x):
+class Pageswtz(Page1_x):
     title = "商务团组"
 
 
-class Page1_9(Page1_x):
+class Pagelftz(Page1_x):
     title = "来访团组"
 
 
 class Pagejzz(Page1_x):
     title = "居住证人员"
     model = JZZ
-    summary = {'姓名': 'nickname', '性别': 'sex', '身份证号码': 'id_card'}
+    summary = {
+        '姓名': 'nickname',
+        '性别': 'sex',
+        '身份证号码': 'id_card'
+    }
