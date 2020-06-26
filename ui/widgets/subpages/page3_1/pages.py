dee3caa7635771c069d79b3c72733ca1843ea250
taoting <335354289@qq.com>
2020-06-26 08:00:36 +0800

    新增显示字段

--- ui/widgets/subpages/page3_1/pages.py
+++ ui/widgets/subpages/page3_1/pages.py
@@ -5,3 +5,8 @@ from ui.page_elements.search_page import SearchPage
 class ChoicePage(SearchPage):
     title = "查询结果"
     model = RS
+    summary = {
+        '姓名': 'nickname',
+        '性别': 'sex',
+        '出生日期': 'birth'
+    }
