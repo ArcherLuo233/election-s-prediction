c44a494de57c941ac1a526b1fa0a70f5ecd43a05
jiubei0408 <757795210@qq.com>
2020-07-17 11:28:39 +0800

    新增字段

--- ui/widgets/subpages/page3/__init__.py
+++ ui/widgets/subpages/page3/__init__.py
@@ -6,11 +6,12 @@ from ui.page_elements.search_page import SearchPage
 class Page3(SearchPage):
     model = JG
     title = "机构"
-    summary = {
-        '理事': 'director',
-        '监事': 'supervisor',
-        '代表': 'representative'
-    }
+    summary = [
+        '名称',
+        '理事',
+        '监事',
+        '代表'
+    ]
 
     def open_detail(self, enable: bool, data):
         from libs.page_magager import PageManager
